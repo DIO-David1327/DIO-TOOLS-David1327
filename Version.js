0b8e778662d14f7b3c18eabbaae93d80c1ba6dad
David1327 <53765374+DIO-David1327@users.noreply.github.com>
2021-08-30 22:54:53 +0200

    Create Version.js

---
+++ Version.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         DIO-TOOLS-David1327-version
+// @description  control version of DIO-TOOLS-David1327
+// @author       David1327
+// ==/UserScript==
+
+var dio_latest_version = '4.23';
