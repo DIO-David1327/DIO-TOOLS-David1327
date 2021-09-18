037eef36877013cc55a9c49e49a2b3e3b94134df
David1327 <53765374+DIO-David1327@users.noreply.github.com>
2021-09-18 22:40:13 +0200

    Update detailData.js

--- Donations/detailData.js
+++ Donations/detailData.js
@@ -1,75 +1,75 @@
 var data_month = ([
-['2020/09', 15, 18,72],
-['2020/10', 15, 18,32],
-['2020/11', 15, 3,67],
-['2020/12', 15, 37,97],
-['2021/01', 15, 9,36],
-['2021/02', 15, 19,07],
-['2021/03', 15, 51,15],
+['2020/09', 15, 18.72],
+['2020/10', 15, 18.32],
+['2020/11', 15, 3.67],
+['2020/12', 15, 37.97],
+['2021/01', 15, 9.36],
+['2021/02', 15, 19.07],
+['2021/03', 15, 51.15],
 ['2021/04', 15, 0],
-['2021/05', 15, 19,07],
-['2021/06', 15, 4,5],
+['2021/05', 15, 19.07],
+['2021/06', 15, 4.5],
 ['2021/07', 15, 0],
-['2021/08', 15, 13,66],
+['2021/08', 15, 13.66],
 ]);
 
 var detailData = {
 '2020/01': [
-{'dat': '2020-01-28', 'inc':9,36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
-]'2020/02': [
-{'dat': '2020-02-11', 'inc':9,36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
-]'2020/03': [
-{'dat': '2020-03-01', 'inc':4,5, 'who': 'Kornelia M', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-14', 'inc':9,36, 'who': 'etienne1306', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-15', 'inc':5,5, 'who': 'Max P', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-16', 'inc':9,36, 'who': 'Doris H', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-19', 'inc':5,04, 'who': 'Annette H', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-19', 'inc':9,36, 'who': 'Christiane g', 'pays': ['DE','Allemand']},
-{'dat': '2020-03-24', 'inc':9,16, 'who': 'Ines L', 'pays': ['CH','Suisse']},
-{'dat': '2020-03-24', 'inc':9,36, 'who': 'Andreas A', 'pays': ['DE','Allemand']},
-]'2020/04': [
-{'dat': '2020-04-12', 'inc':4,5, 'who': 'Kallerberg', 'pays': ['DE','Allemand']},
-{'dat': '2020-04-30', 'inc':4,5, 'who': 'Antonio A', 'pays': ['ES','Espagne']},
-]'2020/05': [
-{'dat': '2020-05-15', 'inc':9,36, 'who': 'Andreas S', 'pays': ['DE','Allemand']},
-{'dat': '2020-05-27', 'inc':19,07, 'who': 'kanokwan s', 'pays': ['DE','Allemand']},
-]'2020/06': [
-{'dat': '2020-06-10', 'inc':4,5, 'who': 'Swen A', 'pays': ['DE','Allemand']},
-]'2020/07': [
-{'dat': '2020-07-06', 'inc':4,5, 'who': 'Raul G', 'pays': ['ES','Espagne']},
-{'dat': '2020-07-17', 'inc':47,2, 'who': 'Nepomuk P', 'pays': ['CH','Suisse']},
-]'2020/08': [
-]'2020/09': [
-{'dat': '2020-09-07', 'inc':9,36, 'who': 'Christian P', 'pays': ['DE','Allemand']},
-{'dat': '2020-09-07', 'inc':9,36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
-]'2020/10': [
-{'dat': '2020-10-02', 'inc':9,16, 'who': 'Ines L', 'pays': ['DE','Suisse']},
-{'dat': '2020-10-24', 'inc':9,16, 'who': 'Eric A', 'pays': ['TG','Togo']},
-]'2020/11': [
-{'dat': '2020-11-17', 'inc':2,08, 'who': 'Martin G', 'pays': ['DE','Allemand']},
-{'dat': '2020-11-25', 'inc':1,59, 'who': 'Marie-Laure D', 'pays': ['FR','France']},
-]'2020/12': [
-{'dat': '2020-12-01', 'inc':5,04, 'who': 'Susi K', 'pays': ['EE','Estonie']},
-{'dat': '2020-12-01', 'inc':9,36, 'who': 'Ulla R', 'pays': ['SE','Suede']},
-{'dat': '2020-12-02', 'inc':19,07, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
-{'dat': '2020-12-24', 'inc':4,5, 'who': 'Mateusz O', 'pays': ['PL','Pologne']},
-]'2021/01': [
-{'dat': '2021-01-21', 'inc':9,36, 'who': 'Jean P', 'pays': ['FR','France']},
-]'2021/02': [
-{'dat': '2021-02-04', 'inc':19,07, 'who': 'Artur Z', 'pays': ['PL','Pologne']},
-]'2021/03': [
-{'dat': '2021-03-06', 'inc':4,4, 'who': 'Therese S', 'pays': ['CZ','Tchèque']},
-{'dat': '2021-03-18', 'inc':9,16, 'who': 'Eli M', 'pays': ['AU','Australie']},
-{'dat': '2021-03-22', 'inc':19,07, 'who': 'Sven K', 'pays': ['DE','Allemand']},
-{'dat': '2021-03-28', 'inc':9,36, 'who': 'Uwe J', 'pays': ['DE','Allemand']},
-{'dat': '2021-03-31', 'inc':9,16, 'who': 'Eli M', 'pays': ['AU','Australie']},
-]'2021/04': [
-]'2021/05': [
-{'dat': '2021-05-03', 'inc':19,07, 'who': 'Elwira G', 'pays': ['DE','Allemand']},
-]'2021/06': [
-{'dat': '2021-06-28', 'inc':4,5, 'who': 'Thomas C', 'pays': [' ',' ']},
-]'2021/07': [
-]'2021/08': [
-{'dat': '2021-08-19', 'inc':4,5, 'who': 'Denai', 'pays': [' ',' ']},
-{'dat': '2021-08-20', 'inc':9,16, 'who': 'elifoxo', 'pays': [' ',' ']},
-]};
+{'dat': '2020-01-28', 'inc':9.36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
+],'2020/02': [
+{'dat': '2020-02-11', 'inc':9.36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
+],'2020/03': [
+{'dat': '2020-03-01', 'inc':4.5, 'who': 'Kornelia M', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-14', 'inc':9.36, 'who': 'etienne1306', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-15', 'inc':5.5, 'who': 'Max P', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-16', 'inc':9.36, 'who': 'Doris H', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-19', 'inc':5.04, 'who': 'Annette H', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-19', 'inc':9.36, 'who': 'Christiane g', 'pays': ['DE','Allemand']},
+{'dat': '2020-03-24', 'inc':9.16, 'who': 'Ines L', 'pays': ['CH','Suisse']},
+{'dat': '2020-03-24', 'inc':9.36, 'who': 'Andreas A', 'pays': ['DE','Allemand']},
+],'2020/04': [
+{'dat': '2020-04-12', 'inc':4.5, 'who': 'Kallerberg', 'pays': ['DE','Allemand']},
+{'dat': '2020-04-30', 'inc':4.5, 'who': 'Antonio A', 'pays': ['ES','Espagne']},
+],'2020/05': [
+{'dat': '2020-05-15', 'inc':9.36, 'who': 'Andreas S', 'pays': ['DE','Allemand']},
+{'dat': '2020-05-27', 'inc':19.07, 'who': 'kanokwan s', 'pays': ['DE','Allemand']},
+],'2020/06': [
+{'dat': '2020-06-10', 'inc':4.5, 'who': 'Swen A', 'pays': ['DE','Allemand']},
+],'2020/07': [
+{'dat': '2020-07-06', 'inc':4.5, 'who': 'Raul G', 'pays': ['ES','Espagne']},
+{'dat': '2020-07-17', 'inc':47.2, 'who': 'Nepomuk P', 'pays': ['CH','Suisse']},
+],'2020/08': [
+],'2020/09': [
+{'dat': '2020-09-07', 'inc':9.36, 'who': 'Christian P', 'pays': ['DE','Allemand']},
+{'dat': '2020-09-07', 'inc':9.36, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
+],'2020/10': [
+{'dat': '2020-10-02', 'inc':9.16, 'who': 'Ines L', 'pays': ['DE','Suisse']},
+{'dat': '2020-10-24', 'inc':9.16, 'who': 'Eric A', 'pays': ['TG','Togo']},
+],'2020/11': [
+{'dat': '2020-11-17', 'inc':2.08, 'who': 'Martin G', 'pays': ['DE','Allemand']},
+{'dat': '2020-11-25', 'inc':1.59, 'who': 'Marie-Laure D', 'pays': ['FR','France']},
+],'2020/12': [
+{'dat': '2020-12-01', 'inc':5.04, 'who': 'Susi K', 'pays': ['EE','Estonie']},
+{'dat': '2020-12-01', 'inc':9.36, 'who': 'Ulla R', 'pays': ['SE','Suede']},
+{'dat': '2020-12-02', 'inc':19.07, 'who': 'Christiane G', 'pays': ['DE','Allemand']},
+{'dat': '2020-12-24', 'inc':4.5, 'who': 'Mateusz O', 'pays': ['PL','Pologne']},
+],'2021/01': [
+{'dat': '2021-01-21', 'inc':9.36, 'who': 'Jean P', 'pays': ['FR','France']},
+],'2021/02': [
+{'dat': '2021-02-04', 'inc':19.07, 'who': 'Artur Z', 'pays': ['PL','Pologne']},
+],'2021/03': [
+{'dat': '2021-03-06', 'inc':4.4, 'who': 'Therese S', 'pays': ['CZ','Tchèque']},
+{'dat': '2021-03-18', 'inc':9.16, 'who': 'Eli M', 'pays': ['AU','Australie']},
+{'dat': '2021-03-22', 'inc':19.07, 'who': 'Sven K', 'pays': ['DE','Allemand']},
+{'dat': '2021-03-28', 'inc':9.36, 'who': 'Uwe J', 'pays': ['DE','Allemand']},
+{'dat': '2021-03-31', 'inc':9.16, 'who': 'Eli M', 'pays': ['AU','Australie']},
+],'2021/04': [
+],'2021/05': [
+{'dat': '2021-05-03', 'inc':19.07, 'who': 'Elwira G', 'pays': ['DE','Allemand']},
+],'2021/06': [
+{'dat': '2021-06-28', 'inc':4.5, 'who': 'Thomas C', 'pays': [' ',' ']},
+],'2021/07': [
+],'2021/08': [
+{'dat': '2021-08-19', 'inc':4.5, 'who': 'Denai', 'pays': [' ',' ']},
+{'dat': '2021-08-20', 'inc':9.16, 'who': 'elifoxo', 'pays': [' ',' ']},
+],};
